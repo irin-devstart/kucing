dc148c3983b57f1ce380edb506447363ed57ce32
irin-devstart <irin.devstart@gmail.com>
2024-09-16 21:03:01 +0700

    Finishing code

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View, Image } from "react-native";
 
-export default function App() {
+export default function Statustbsr() {
+  const [a, ubahNilai] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onPress={() => alert("HAHAHAH")}>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={{ uri: "https://facebook.github.io/react/img/logo_og.png" }}
+      />
       <StatusBar style="auto" />
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1, width: 100 }}
+        onChangeText={(e) => ubahNilai(e)}
+      />
+      <Text style={{ fontSize: 20, color: "blue", fontWeight: "400" }}>
+        {a}
+      </Text>
+      <Button
+        title="Klik Disini"
+        color="red"
+        onPress={() => ubahNilai(a + 1)}
+        backgroundColor="#000"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -14,8 +33,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    color: "blue",
+    fontWeight: "400",
   },
 });
